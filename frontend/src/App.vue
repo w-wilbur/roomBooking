ac9f4e0b2edd642fd50c94ffd2a1d6224d25c4b7
wilbur <wilbur.wang@mobilenowgroup.com>
2017-11-07 17:10:33 +0800

    gob up signIn page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
     <router-view/>
+    <el-button @click="visible = true">按钮</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data: ()=> ({ visible: false })
 }
 </script>
 
--- frontend/src/pages/sign-in.vue
+++ frontend/src/pages/sign-in.vue
@@ -1,9 +1,76 @@
 <template>
-	<div>登录页</div>
+	<div>
+		<el-container>
+			<el-main>
+				<div class='pannel'>
+					<p style="font-size:20px;">Mobile Now meeting room</p>
+					<el-tabs type="border-card">
+  						<el-tab-pane label="登录">
+  							<el-form ref='form'>
+  								<el-form-item>
+  									<el-input type="text" v-model='form.name' placeholder="请输入用户名"></el-input>
+  								</el-form-item>
+  								<el-form-item>
+  									<el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+  								</el-form-item>
+  								<el-form-item style="margin-bottom:10px;">
+  									<el-button type="primary" @click="signIn" style="width:100%;">登录</el-button>
+  								</el-form-item>
+  							</el-form>
+  						</el-tab-pane>
+  						<el-tab-pane label="注册">
+  							<el-form ref='form'>
+  								<el-form-item>
+  									<el-input type="text" v-model='form.name' placeholder="请输入用户名"></el-input>
+  								</el-form-item>
+  								<el-form-item>
+  									<el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+  								</el-form-item>
+  								<el-form-item>
+  									<el-input type="password" v-model="form.repassword" placeholder="请再次输入密码"></el-input>
+  								</el-form-item>
+  								<el-form-item style="margin-bottom:10px;">
+  									<el-button type="primary" @click="register" style="width:100%;">注册</el-button>
+  								</el-form-item>
+  							</el-form>
+  						</el-tab-pane>
+					</el-tabs>
+				</div>
+			</el-main>
+		</el-container>
+	</div>
 </template>
 
 <script>
 	export default {
-		
+		data() {
+			return {
+				form: {
+					name: '',
+					password: '',
+					repassword: ''
+				}
+			}
+		},
+		methods: {
+			signIn() {
+				console.log(this.form.name);
+			},
+			register() {
+				console.log("注册成功！");
+			}
+		}
 	}
 </script>
+<style scoped>
+
+	.pannel{
+		/*width: 400px;
+		margin: 0 auto;
+		padding: 20px;
+		border: 1px solid #409EFF;*/
+		width: 500px;
+		margin: 0 auto;
+		margin-top: 60px;
+	}
+</style>
